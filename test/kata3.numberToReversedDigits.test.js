12ec3edab1662dfefe7970d972408055fb2a214c
Josh Bamford <joshbamford09@gmail.com>
2022-10-18 21:17:24 +0100

    commented tip

--- test/kata3.numberToReversedDigits.test.js
+++ test/kata3.numberToReversedDigits.test.js
@@ -6,4 +6,4 @@ describe("numberToReversedDigits", () => {
     expect(numberToReversedDigits(4983)).toEqual([3, 8, 9, 4]);
     expect(numberToReversedDigits(212)).toEqual([2, 1, 2]);
   });
-});
+});//Used toEqual as toBe doesnt work with Arrays,objects etc due to it looking for the exact Array rather than something that contains the samr values.
