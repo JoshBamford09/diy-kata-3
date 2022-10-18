09b215bda936e4144a58be55cc0a1e07f48d1ebd
Josh Bamford <joshbamford09@gmail.com>
2022-10-18 20:48:34 +0100

    Completed BooleanToString Test Kata

--- test/kata2.booleanToWord.test.js
+++ test/kata2.booleanToWord.test.js
@@ -1,5 +1,8 @@
 const { booleanToWord } = require("../src");
 
 describe("booleanToWord", () => {
-  // how do we create specs again???
+  it("Returns a string of 'Yes' if given TRUE, and 'No' if given FALSE", () => {
+    expect(booleanToWord(true)).toBe("Yes");
+    expect(booleanToWord(false)).toBe("No");
+  });
 });
