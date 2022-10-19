1f61e3fd35c375a38516cfe93d1871ae1536d321
Josh Bamford <joshbamford09@gmail.com>
2022-10-19 19:44:42 +0100

    Completed HumanCatDogYears kata

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,5 +1,9 @@
 const humanCatDogYears = (number) => {
-  const
+  const humanYears = number;
+  const catYears = 24 + 4 * (humanYears - 2);
+  const dogYears = catYears + humanYears - 2;
+  const newArray = [humanYears, catYears, dogYears];
+  return newArray;
 };
 
 module.exports = humanCatDogYears;
