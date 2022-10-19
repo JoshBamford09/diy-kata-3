2507908cc7a3718b405ded0c891b89e2f1d8daff
Josh Bamford <joshbamford09@gmail.com>
2022-10-19 20:03:46 +0100

    completed reachDestination kata

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,7 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+  const timeToDestination = distance / speed;
+  const numberRounded = (timeToDestination * 2).toFixed() / 2;
+  return `I should be there in ${numberRounded} hours.`;
+};
 
 module.exports = reachDestination;
