2ef2b72f1575b61557bec52d4ec75aa376eeaf15
Josh Bamford <joshbamford09@gmail.com>
2022-10-20 18:35:51 +0100

    Made headway into joinNames kata

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,8 @@
-const joinNames = (namesObj) => {};
+const joinNames = (namesObj) => {
+  const listOfNames = namesObj.map((person) => person.name);
+  return listOfNames;
+  // want to create a for loop that assigns each index of the above array to a new string of name,
+  // exept for the last index whihc will be added onto the string seperatley
+};
 
 module.exports = joinNames;
